726e3dfe8230e7d32854227ea414e7119c495a46
kitou88C39 <kanfutrooper@icloud.com>
2023-05-30 17:51:06 +0900

    first commit

---
+++ src/main/java/Inquiry_form/InquiryFormApplication.java
@@ -0,0 +1,13 @@
+package Inquiry_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Inquiry_form/InquiryFormApplicationTests.java
@@ -0,0 +1,13 @@
+package Inquiry_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiryFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
